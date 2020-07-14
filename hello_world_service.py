229529750ab95c9e5f403a0c6fab7f553f2afd96
Clodo <claudiobidau@gmail.com>
2020-07-13 21:12:38 -0300

    Revert "Merge branch 'release/v0.0.2'"
    This reverts commit 94a1edb67ee296611c27d2a26266f4521ff03fdc, reversing
    changes made to 6e5df83b7c58f7e5f94186f58be4bb443831fcad.

--- hello_world_service.py
+++ hello_world_service.py
@@ -1,9 +1,6 @@
 def print_message(message):
     print(message)
 
-def print_another_message():
-    print("Won't you tell me your name?")
-
 
 if __name__ == '__main__':
     print_message('Hello I love you')
