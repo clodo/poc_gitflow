eec7f405236bf8309e3e5e19887e43813115717b
Clodo <claudiobidau@gmail.com>
2020-07-13 21:28:15 -0300

    Add difference numbers method.

--- hello_world_service.py
+++ hello_world_service.py
@@ -4,6 +4,9 @@ def print_message(message):
 def sum_numbers(num_a, num_b):
   return num_a + num_b
 
+def difference(num_a, num_b):
+  return num_a - num_b
+
 
 if __name__ == '__main__':
     print_message('Hello I love you')
